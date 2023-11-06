424b907093d22804d62500255b2ee82b157419cc
SipanAkcan <67048219+SipanAkcan@users.noreply.github.com>
2023-11-06 22:16:16 +0300

    Update AutoConstants.java

--- src/main/java/frc/robot/auto/AutoConstants.java
+++ src/main/java/frc/robot/auto/AutoConstants.java
@@ -6,7 +6,7 @@ public class AutoConstants {
 
     public static final double REDUCTION_GEAR = 1;
     public static final double WHEEL_DIAMETER_INCHES = 6;
-    public static final double ENCODER_PPR = 1024;
+    public static final double ENCODER_PPR = 42;
 
     public static final double K_DRIVE_TICK_2_FEET = REDUCTION_GEAR / Math.PI * WHEEL_DIAMETER_INCHES * ENCODER_PPR / 12;
 
