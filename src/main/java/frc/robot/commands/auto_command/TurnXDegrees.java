897ad3db47ae2c0af2883bfbc925826587edf32b
Burak Talha <alptng72@gmail.com>
2023-11-08 18:49:45 +0300

    Update unnecessary definitions

--- src/main/java/frc/robot/commands/auto_command/TurnXDegrees.java
+++ src/main/java/frc/robot/commands/auto_command/TurnXDegrees.java
@@ -12,11 +12,10 @@ public class TurnXDegrees extends CommandBase {
   DriveSubsystem driveSubsystem;
   double setpoint;
   double speed;
-  public TurnXDegrees(DriveSubsystem driveSubsystem, double setpoint, double speed) {
+  public TurnXDegrees(DriveSubsystem driveSubsystem, double setpoint) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.driveSubsystem = driveSubsystem;
     this.setpoint = setpoint;
-    this.speed = speed;
   }
 
   // Called when the command is initially scheduled.
@@ -26,7 +25,7 @@ public class TurnXDegrees extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveSubsystem.turnXDegrees(setpoint, speed);
+    driveSubsystem.turnXDegrees(setpoint);
   }
 
   // Called once the command ends or is interrupted.
